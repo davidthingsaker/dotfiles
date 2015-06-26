705ff3cb1fa9090431f459aca2e3437abb811859
David Thingsaker <davidthingsaker@gmail.com>
2015-06-26 16:40:34 +0100

    updates to readme and bootstrap.sh

--- README.md
+++ README.md
@@ -12,7 +12,42 @@ The idea of the bootstrap script is to run all the other stuff in turn so that y
 The script runs a little like this:
 
 1. Installs oh my zsh, then copies the customisations over from the `zsh` directory.
-2. Runs Thoughtbots script to install all the [Homebrew](http://brew.sh) and [Cask](http://caskroom.io) packages and apps.
+2. Runs Thoughtbots script to install all the [Homebrew](http://brew.sh) and [Cask](http://caskroom.io) packages and apps. I have tweeked the packages that Thoughtbot installs via Homebrew and have added my Cask apps to the `.laptop.local.sh` file.
+    * Homebrew Packages
+        * git
+        * redis
+        * imagemagick
+        * hub
+        * node
+        * rbenv
+        * ruby-build
+    * Cask Applications
+        * Dropbox
+        * Google- Chrome
+        * Firefox
+        * smcFancControl
+        * Tower
+        * phpStorm
+        * Sublime Text
+        * Evernote
+        * VyprVPN
+        * Chrome Remote Desktop Host
+        * VLC
+        * Virtualbox
+        * Vagrant
+        * Skype
+        * Keka
+        * SequelPro
+        * Sonarr
+        * SABnzbd
+        * Couch Potato
+        * Tor Browser
+        * Transmission
+        * Plex Media Server
+        * Plex Home Theater
+        * tree
+        * watch
+
 3. Install [Homestead](http://laravel.com/docs/5.1/homestead), a vagrant box by Laravel.
 4. Installs [composer](http://laravel.com/docs/5.1/homestead)
 5. Runs the dotfiles
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,33 +5,81 @@
 # Ask for sudo password
 sudo -v
 
-# Install Oh My ZSH
-echo "Installing ZSH"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+homestead_is_upgradable() {
+    echo 'Checking if you are running latest version of Homestead'
+    vagrant box update --box laravel/homestead | grep -q "latest version"
+}
 
-# Move my customisations into place
+homestead_is_installed() {
+    echo 'Checking if Homestead is already installed'
+    vagrant box list | grep -q "homestead"
+}
+
+install_oh_my_zsh() {
+    echo 'Installing Oh My ZSH'
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+# Install Oh My ZSH
+# First check you have ZSH, if you're on Mac OS X you should already have it
+if type zsh > /dev/null; then
+    echo 'You have ZSH installed already'
+    if [ ! -d ~/.oh-my-zsh ]; then
+        install_oh_my_zsh
+    else
+        echo 'Looks like you already have Oh My ZSH installed as well'
+    fi
+else
+    echo "Installing ZSH"
+    apt-get install zsh
+    install_oh_my_zsh
+fi
+
+echo "Moving ZSH customisations into place"
+Move my customisations into place
 rm ~/.zshrc
 cp zsh/.zshrc ~
 cp zsh/davidthingsaker.zsh ~/.oh-my-zsh/custom
 cp zsh/davidthingsaker.zsh-theme ~/.oh-my-zsh/custom/themes
 
+echo "Running the Thoughtbot script and .laptop.local"
+echo "This will install all the apps"
 # Install Homebrew, Cask and all the packages using Thoughtbots script
-sh mac 2>&1 | tee ~/laptop.log
+sh laptop/mac 2>&1 | tee ~/laptop.log
 
+echo "Installing Homestead"
 # Install Homestead for all the Laravel apps
-vagrant box add laravel/homestead
-
-# Install Composer and add to path
-curl -sS https://getcomposer.org/installer | php
-
-if [ ! -d "/usr/local/bin" ]; then
-  mkdir /usr/local/bin
+if homestead_is_installed; then
+    if homestead_is_upgradable; then
+        echo 'Upgrading the homestead box to latest version'
+        vagrant box update --box laravel/homestead
+    else
+        echo 'You are already running the most upto date version. Have a beer! \xF0\x9f\x8d\xba'
+    fi
+else
+    vagrant box add laravel/homestead
 fi
 
-mv composer.phar /usr/local/bin/composer
+if type composer > /dev/null; then
+    echo 'Looks like you already have composer installed'
+    sudo /usr/bin/composer self-update
+else
+    echo "Installing Composer"
+    Install Composer and add to path
+    curl -sS https://getcomposer.org/installer | php
 
+    echo "Adding composer to PATH"
+    According to composers notes, Yosemite does not have /usr/local/bin by default. Check and create it if not.
+    if [ ! -d /usr/local/bin ]; then
+      mkdir /usr/local/bin
+    fi
+
+    mv composer.phar /usr/local/bin/composer
+fi
+
+echo "Running the dotfiles"
 # Run the dotfiles
-./dotfiles/osx/defaults.sh
+sh dotfiles/osx/defaults.sh
 
 
 
