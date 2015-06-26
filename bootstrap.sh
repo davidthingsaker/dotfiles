e3d8367de89f2585f6283c66c93f42baca07e25c
David Thingsaker <davidthingsaker@gmail.com>
2015-06-26 11:35:53 +0100

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Bootstrap file
+
+# Ask for sudo password
+sudo -v
+
+# Install Oh My ZSH
+echo "Installing ZSH"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Move my customisations into place
+rm ~/.zshrc
+cp zsh/.zshrc ~
+cp zsh/davidthingsaker.zsh ~/.oh-my-zsh/custom
+cp zsh/davidthingsaker.zsh-theme ~/.oh-my-zsh/custom/themes
+
+# Install Homebrew, Cask and all the packages
+sh mac 2>&1 | tee ~/laptop.log
+
+# Install Homestead for all the Laravel apps
+vagrant box add laravel/homestead
+
+# Install Composer and add to path
+curl -sS https://getcomposer.org/installer | php
+
+if [ ! -d "/usr/local/bin" ]; then
+  mkdir /usr/local/bin
+fi
+
+mv composer.phar /usr/local/bin/composer
+
+# Run the dotfiles
+./dotfiles/osx/defaults.sh
+
+
+
